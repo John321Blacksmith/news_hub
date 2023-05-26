f1283efeb2a64106ea7db96186f42694d8da41a7
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-05-26 17:50:15 +0300

    the signup system has been covered by tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy, reverse
 
 
 # Create your tests here.
@@ -10,31 +10,39 @@ class SignUpTest(TestCase):
 	These tests check the work of the
 	signup system.
 	"""
-
-
-	def test_model_data(self):
-		"""
-		This test checks if the model
-		system works well.
-		"""
-		pass
-
 	def test_url_exists_at_correct_loc(self):
 		"""
 		This test checks if the
 		the url works in a common way.
 		"""
-		pass
+		response = self.client.get('/accounts/signup/')
+		self.assertEqual(response.status_code, 200)
 
 	def test_url_available_via_name(self):
 		"""
 		This test checks if the url
 		is available through the pattern name.
 		"""
-		pass
+		response = self.client.get(reverse('signup'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'registration/signup.html')
 
-	def test_signup_page(self):
+	def test_signup_form(self):
 		"""
 		This test checks the work
-		of the rendered template.
+		of the rendered form.
 		"""
+		response = self.client.post(
+				reverse('signup'),
+				{
+					'username': 'TestUser',
+					'email': 'test@mail.com',
+					'password1': '1234hghdsfahgeywegje64fhjkdhzahgsf',
+					'password2': '1234hghdsfahgeywegje64fhjkdhzahgsf',
+				},
+			)
+		
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(get_user_model().objects.all().count(), 1)
+		self.assertEqual(get_user_model().objects.all()[0].username, 'TestUser')
+		self.assertEqual(get_user_model().objects.all()[0].email, 'test@mail.com')
